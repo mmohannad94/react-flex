ff06149cc0da9c30b050e9ab2df3d4105f4a357d
Mohannad <s.m.9.4.1994@gmail.com>
2021-03-10 23:16:25 +0200

    format App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import './App.css';
 
 function App() {
-  return (
-      <section className="container-fluid d-flex flex-column h-100vh">
+    return (
+        <section className="container-fluid d-flex flex-column h-100vh">
 
-        <header className="flex-fill row">
-          <div className="py-3 border align-items-center col bg-primary border-primary">
-            <h2 className="text-center text-white">Header</h2>
-          </div>
-        </header>
-        <section className="flex-fill row">
-          <aside
-              className="order-2 py-3 border align-items-center col-12 order-md-1 col-md-3 bg-primary border-primary">
-            <h2 className="text-center text-white">Sidebar1</h2>
-          </aside>
-          <main className="order-1 py-3 border col-12 order-md-2 col-md-6 bg-primary border-primary">
-            <h2 className="text-center text-white">Main</h2>
-          </main>
-          <aside className="order-3 py-3 border col-12 order-md-3 col-md-3 bg-primary border-primary">
-            <h2 className="text-center text-white">Sidebar2</h2>
-          </aside>
+            <header className="flex-fill row">
+                <div className="py-3 border align-items-center col bg-primary border-primary">
+                    <h2 className="text-center text-white">Header</h2>
+                </div>
+            </header>
+            <section className="flex-fill row">
+                <aside
+                    className="order-2 py-3 border align-items-center col-12 order-md-1 col-md-3 bg-primary border-primary">
+                    <h2 className="text-center text-white">Sidebar1</h2>
+                </aside>
+                <main className="order-1 py-3 border col-12 order-md-2 col-md-6 bg-primary border-primary">
+                    <h2 className="text-center text-white">Main</h2>
+                </main>
+                <aside className="order-3 py-3 border col-12 order-md-3 col-md-3 bg-primary border-primary">
+                    <h2 className="text-center text-white">Sidebar2</h2>
+                </aside>
+            </section>
+            <footer className="flex-fill row">
+                <div className="py-3 border align-items-center col bg-primary border-primary">
+                    <h2 className="text-center text-white">Footer</h2>
+                </div>
+            </footer>
         </section>
-        <footer className="flex-fill row">
-          <div className="py-3 border align-items-center col bg-primary border-primary">
-            <h2 className="text-center text-white">Footer</h2>
-          </div>
-        </footer>
-      </section>
-  );
+    );
 }
 
 export default App;
